36919a5308f3007b03c641d74985375a55d706d3
Yannick Epstein <yannick.epstein@gmail.com>
2020-08-03 17:58:50 +0200

    Employer Frontend
    Added buefy and axios dependencies

--- employer-frontend/src/main.js
+++ employer-frontend/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import Buefy from 'buefy'
 import App from './App.vue'
 
+import 'buefy/dist/buef.css'
+
 Vue.config.productionTip = false
+Vue.use(Buefy)
 
 new Vue({
   render: h => h(App),
